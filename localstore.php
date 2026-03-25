6cb20d286c2d02b7f8d9d5705a3e0856d2a851c2
Saim <saimimran.dev@gmail.com>
2026-03-25 19:21:09 +0500

    ECC

---
+++ localstore.php
@@ -0,0 +1,122 @@
+<?php
+
+session_start();
+
+include("includes/db.php");
+include("includes/header.php");
+include("functions/functions.php");
+include("includes/main.php");
+
+?>
+<!-- MAIN -->
+<main>
+  <!-- HERO -->
+  <div class="nero">
+    <div class="nero__heading">
+      <span class="nero__bold">Local </span>Stores
+    </div>
+    <p class="nero__text">
+    </p>
+  </div>
+</main>
+
+
+<div id="content">
+  <div class="container-fluid">
+    <div class="col-md-12">
+
+      <div class="services row mt-5">
+        <?php
+
+        $get_store = "select * from store";
+
+        $run_store = mysqli_query($con, $get_store);
+
+        while ($row_store = mysqli_fetch_array($run_store)) {
+
+          $store_id = $row_store['store_id'];
+
+          $store_title = $row_store['store_title'];
+
+          $store_image = $row_store['store_image'];
+
+          $store_desc = substr($row_store['store_desc'], 0, 400);
+
+          $store_button = $row_store['store_button'];
+
+          $store_url = $row_store['store_url'];
+
+
+        ?>
+
+          <div class="col-lg-4 col-md-4"><!-- col-lg-4 col-md-4 Starts -->
+
+            <div class="panel panel-primary"><!-- panel panel-primary Starts -->
+
+              <div class="panel-heading"><!-- panel-heading Starts -->
+
+                <h3 class="panel-title" align="center">
+
+                  <?php echo $store_title; ?>
+
+                </h3>
+
+              </div><!-- panel-heading Ends -->
+
+              <div class="panel-body"><!-- panel-body Starts -->
+
+                <img src="admin/store_images/<?php echo $store_image; ?>" class="img-responsive">
+
+                <br>
+
+                <p><?php echo $store_desc; ?></p>
+
+              </div><!-- panel-body Ends -->
+
+              <div class="panel-footer"><!-- panel-footer Starts -->
+
+                <a href="index.php?delete_store=<?php echo $store_id; ?>" class="pull-left">
+
+                  <i class="fa fa-trash-o"></i> Delete
+
+                </a>
+
+                <a href="index.php?edit_store=<?php echo $store_id; ?>" class="pull-right">
+
+                  <i class="fa fa-pencil"></i> Edit
+
+                </a>
+
+                <div class="clearfix"> </div>
+
+              </div><!-- panel-footer Ends -->
+
+            </div><!-- panel panel-primary Ends -->
+
+          </div><!-- col-lg-4 col-md-4 Ends -->
+
+        <?php } ?>
+      </div><!-- services row Ends -->
+
+    </div><!-- col-md-12 Ends -->
+
+
+
+  </div><!-- container Ends -->
+</div><!-- content Ends -->
+
+
+
+<?php
+
+include("includes/footer.php");
+
+?>
+
+<script src="js/jquery.min.js"> </script>
+
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
